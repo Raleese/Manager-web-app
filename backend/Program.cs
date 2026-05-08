8c5a8c32e1234fc05bb8b157d4348a10627421e1
Raleese <titaslr38@gmail.com>
2026-05-08 15:02:16 +0300

    Initial commit

---
+++ backend/Program.cs
@@ -0,0 +1,35 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("AppDatabase"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors("AllowReactApp");
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
