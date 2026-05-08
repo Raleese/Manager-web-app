38d639c820b540a1e3934c1fcc523a2890a8c445
Raleese <titaslr38@gmail.com>
2026-05-09 01:17:59 +0300

    Changes to inventory item class

--- backend/Models/InventoryItem.cs
+++ backend/Models/InventoryItem.cs
@@ -5,10 +5,8 @@ public class InventoryItem
     public int Id { get; set; }
     public ItemType Type { get; set; }
     public string Identifier { get; set; } = "";
-    public int Quantity { get; set; }
     public string Comment { get; set; } = "";
-    public DateTime PurchaseDate { get; set; }
-    public bool IsAssigned { get; set; } = true;
-    public int? UserId { get; set; }
     public User? User { get; set; }
+    public DateTime? PurchaseDate { get; set; }
+    public bool IsAssigned { get; set; } = true;
 }
