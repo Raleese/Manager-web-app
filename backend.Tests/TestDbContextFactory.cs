52608fab4634b15d9043a5eb4b6c421526edfd7c
Raleese <titaslr38@gmail.com>
2026-05-15 20:08:03 +0300

    Added database context helper

---
+++ backend.Tests/TestDbContextFactory.cs
@@ -0,0 +1,16 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Tests;
+
+internal static class TestDbContextFactory
+{
+    public static AppDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        return new AppDbContext(options);
+    }
+}
